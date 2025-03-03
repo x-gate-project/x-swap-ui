ac7df3750001043a793dc17baba1d3d07099327d
nguyenduyhust <nguyenduy.hust@gmail.com>
2025-03-03 19:19:10 +0700

    Add REACT_APP_DEFAULT_TOKEN_LIST_URL

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,6 @@
 // the Uniswap Default token list lives here
 export const DEFAULT_TOKEN_LIST_URL =
+  process.env.REACT_APP_DEFAULT_TOKEN_LIST_URL ||
   'https://raw.githubusercontent.com/x-gate-project/x-swap-token-list/main/tokenlist.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
