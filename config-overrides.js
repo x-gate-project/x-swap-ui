e52154425b87110442f86a2768eda7095bc98078
cuonghx <cuonghx@gmail.com>
2025-03-11 10:32:00 +0700

    Use react-app-rewired

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,13 @@
-/* config-overrides.js */
+const { override, addBabelPlugin, babelInclude } = require('customize-cra')
+const path = require('path')
 
-module.exports = function override(config, env) {
-  //do stuff with the webpack config...
-  return config
-}
+module.exports = override(
+  // Add plugins to support optional chaining and nullish coalescing
+  addBabelPlugin(require.resolve('@babel/plugin-proposal-optional-chaining')),
+  addBabelPlugin(require.resolve('@babel/plugin-proposal-nullish-coalescing-operator')),
+  // Force Babel to transpile the problematic module in node_modules
+  babelInclude([
+    path.resolve(__dirname, 'src'),
+    path.resolve(__dirname, 'node_modules/@walletconnect/ethereum-provider')
+  ])
+)
